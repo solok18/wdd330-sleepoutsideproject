3c56e6008ffd6449cf5cb5f71792debcac310ad8
solok18 <ksolorzano862@gmail.com>
2025-09-25 21:04:11 -0700

    team week three correction

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `../public/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
@@ -19,5 +19,6 @@ export default class ProductData {
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
+    
   }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,14 @@
-import { getParam, setLocalStorage } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource);
-product.init()
+product.init();
 
 // function addProductToCart(product) {
 //   setLocalStorage("so-cart", product);
