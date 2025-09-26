7281ef1368eb76313ec8b57ae6abbfc1a31613b9
George Quito <george-quitog@hotmail.com>
2025-09-25 19:08:46 -0600

    W04

--- src/js/cart.js
+++ src/js/cart.js
@@ -28,7 +28,7 @@ function renderCartContents() {
   footer.querySelector(".cart-total").textContent = `Total: $${total.toFixed(2)}`;
 }
 
-// Edited for new images
+// Edited for new images :D
 function cartItemTemplate(item, index) {
   const img =
     item.Images?.PrimarySmall ||
